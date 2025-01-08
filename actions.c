08604c84b3bfd07a3b069d6fa8fc5377d0b29f3c
Nicolas5u <nicolasdurand34080@gmail.com>
2025-01-08 11:25:21 +0100

    Update actions.c

--- actions.c
+++ actions.c
@@ -15,6 +15,18 @@
 
 // realiser 1000 test pour par exemple si il mets é au lieu de 2 que cela ne fasse pas de la dé
 
-void vertical (Coup){
-    if (
+
+
+// fonctin qui verifie que le mouvement soit horizontal ou vertical et qui deplace le pion si c'est bon
+void verti_Horiz (Coup){
+    if ((xFrom == xTo) || (yFrom == yTo)){
+        echiquier[xTo][yTo] = echiquier[xFrom][yFrom];
+    }
+}
+
+// fonctin qui verifie que le mouvement soit diagonal et qui deplace le pion si c'est bon
+void diagonal (Coup){
+    if ((xFrom == xTo) || (yFrom == yTo)){
+        echiquier[xTo][yTo] = echiquier[xFrom][yFrom];
+    }
 }
