1e278671486a0acbd27480c51de04a4740014890
noah-ey <noah.eynard@free.fr>
2025-01-08 10:26:48 +0100

    Update actions.c

--- actions.c
+++ actions.c
@@ -1,5 +1,15 @@
-// Vérification si la case de départ contient une pièce
-// Vérification des coups légaux
-// Gérer les actions → Faire bouger les pièces (= les coups)
 
-dddd
+// programme
+// fonction qui ?
+// mouvement vert/horiz
+// mouvement diag
+// verification de la possibilite des demandes (fichier c)
+// mouvement des pieces/prend les pieces si besoin
+// fonction rock
+// situation d'echec
+
+// timer
+// les points des joueur
+// les fonction save (qui permet de revenir en arriere une fois)
+
+// realiser 1000 test pour par exemple si il mets é au lieu de 2 que cela ne fasse pas de la dé
