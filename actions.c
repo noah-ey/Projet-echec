24e4539df67146e593a900cef14cdca219132f6b
Nicolas5u <nicolasdurand34080@gmail.com>
2025-01-08 12:05:43 +0100

    Update actions.c

--- actions.c
+++ actions.c
@@ -15,19 +15,32 @@
 
 // realiser 1000 test pour par exemple si il mets é au lieu de 2 que cela ne fasse pas de la dé
 
-
-
-// fonctin qui verifie que le mouvement soit horizontal ou vertical et qui deplace le pion si c'est bon
-void verti_Horiz (Coup){
-    if ((xFrom == xTo) || (yFrom == yTo)){
-        echiquier[xTo][yTo] = echiquier[xFrom][yFrom];
-    }
-    
+// fonction qui deplace la piece sans condition
+void deplacement(Coup){
+    echiquier[xTo][yTo] = echiquier[xFrom][yFrom];
+    echiquier[xFrom][yFrom].p = vide;
+    return;
 }
-
+// fonctin qui verifie que le mouvement soit vertical et qui deplace le pion si c'est bon
+int verti (Coup){
+    if (yFrom != yTo)){
+        printf("mouvement impossible car il n'est pas verti");
+        return 0;
+    }
+    return 1;
+}
+// fonctin qui verifie que le mouvement soit horizontal et qui deplace le pion si c'est bon
+int horiz (Coup){
+    if ((yFrom != xTo) {
+        printf("mouvement impossible car il n'est pas horiz");
+        return 0;
+    }
+    return 1;
+}
 // fonctin qui verifie que le mouvement soit diagonal et qui deplace le pion si c'est bon
 void diagonal (Coup){
-    if ((xFrom == xTo) || (yFrom == yTo)){
-        echiquier[xTo][yTo] = echiquier[xFrom][yFrom];
+    if ((xTo - xFrom != yTo - yFrom) || (-yTo + xFrom != xTo - yFrom) || (yTo + xFrom != xTo + yFrom)){ // dans la premiere diagonal puis dans la deuxieme diagonal en haut puis dans la deuxieme diagonal en bas
+        return 0;
     }
+    return 1;
 }
