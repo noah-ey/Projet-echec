1e89f426a8de4da8a8dc4306186dd88d2d88c6aa
Nicolas5u <nicolasdurand34080@gmail.com>
2025-01-08 15:43:00 +0100

    Update actions.c

--- actions.c
+++ actions.c
@@ -14,46 +14,44 @@
 // realiser 1000 test pour par exemple si il mets é au lieu de 2 que cela ne fasse pas de la dé
 
 // fonction qui deplace la piece sans condition
-void deplacement(Coup){
+void deplacement(Coup coup){
     echiquier[xTo][yTo] = echiquier[xFrom][yFrom];
     echiquier[xFrom][yFrom].p = vide;
     return;
 }
 // fonctin qui verifie que le mouvement soit vertical
-int verti (Coup){
-    if (yFrom != yTo)){
-        printf("mouvement impossible car il n'est pas verti");
-        return 0;
+int verti (Coup coup){
+    if (yFrom == yTo)){
+        printf("mouvement impossible car il n'est pas verti\n");
+        return 1;
     }
-    return 1;
+    return 0;
 }
 // fonctin qui verifie que le mouvement soit horizontal
-int horiz (Coup){
-    if ((yFrom != xTo) {
-        printf("mouvement impossible car il n'est pas horiz");
-        return 0;
+int horiz (Coup coup){
+    if ((yFrom == xTo) {
+        printf("mouvement impossible car il n'est pas horiz\n");
+        return 1;
     }
-    return 1;
+    return 0;
 }
 // fonctin qui verifie que le mouvement soit diagonal
-int diagonal (Coup){
-    if (xTo - xFrom != yTo - yFrom){  // dans la premiere diagonal
-        return 0;
-    } if (-yTo + xFrom != xTo - yFrom){  // dans la deuxieme diagonal 
-        return 0;
-    } if (yTo + xFrom != xTo + yFrom){  // dans la deuxieme diagonal en bas
-        return 0;
+int diagonal (Coup coup){
+    if (xTo - xFrom == yTo - yFrom){  // dans la premiere diagonal
+        return 1;
+    } if (-yTo + xFrom == xTo - yFrom){  // dans la deuxieme diagonal 
+        return 1;
+    } if (yTo + xFrom == xTo + yFrom){  // dans la deuxieme diagonal en bas
+        return 1;
     }
-    return 1;
+    return 0;
 }
 // fonctin qui verifie que le mouvement soit bon pour le cavalier
 int mouvcavalier (Coup coup){
-    if ((abs (coup.xTo - coup.xFrom) != 2) && (abs (coup.yTo - coup.yFrom) != 1)){
-        return 0;
-    } else if ((abs (coup.xTo - coup.xFrom) != 2) && (abs (coup.yTo - coup.yFrom) != 1)){
-        return 0;
+    if ((abs (coup.xTo - coup.xFrom) == 2) && (abs (coup.yTo - coup.yFrom) == 1) || (abs (coup.xTo - coup.xFrom) == 2) && (abs (coup.yTo - coup.yFrom) == 1)){
+        return 1;
     } 
-    return 1;
+    printf("Mouvement impossible car cela ne correspond pas au cavalier\n");
+    return 0;
 }
-
 
