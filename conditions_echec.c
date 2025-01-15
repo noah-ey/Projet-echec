4df90d9270c1d4a77014da62bee43eea5254913a
Nicolas5u <nicolasdurand34080@gmail.com>
2025-01-15 23:37:08 +0100

    Update conditions_echec.c

--- conditions_echec.c
+++ conditions_echec.c
@@ -10,14 +10,42 @@ Fichier qui regroupe toutes les fonctions gérant les conditions d'échec
 int RoiEnEchec(Partie* partie, Coup coup){
 
   //on stock les coordonné du tableau avant test
-
+  Case suivant = partie->echiquier[coup.yTo][coup.xTo];
+  
   //on bouge le tableau
+  partie->echiquier[coup.yTo][coup.xTo] = partie->echiquier[coup.yFrom][coup.xFrom];
+  partie->echiquier[coup.yFrom][coup.xFrom].p = vide;
 
   //on trouve le roi
+  for(int i = 0; i < NB_LIGNE; i++){
+    for(int j = 0; j < NB_LIGNE; j++){
+      if(partie->echiquier[i][j].p == roi && partie->echiquier[i][j].c == partie->joueur_actif){
+        roiX=i;
+        roiY=j;
+      }
+    }
+  }
 
-  //on test l'echec pour toute les pieces en utilisant int echec = //true si le roi est en echec apres le mouvement
+  //on test l'echec pour toute les pieces en utilisant 
+  //int echec = //true si le roi est en echec apres le mouvement 
+  //car apres on dois remettre le plateau comme avant donc on dois apeller 
+  //une fonction maintenant pour pouvoir renvoyer plus tard le bon return
+  int echec = risquePourRoi(partie, roiX, roiY);
 
-  //on remet le tableau comme avant
+  //on remet le tableau comme avant (même chose mais dans l'autre sens)
+  partie->echiquier[coup.yFrom][coup.xFrom] = partie->echiquier[coup.yTo][coup.xTo];
+  partie->echiquier[coup.yTo][coup.xTo].p = suivant;
 
   return echec; //true si le roi est en echec apres le mouvement 
+}
+
+int risquePourRoi(Partie* partie, int roiX, int roiY){
+  for(int i = 0; i < NB_LIGNE; i++){
+    for(int j = 0; j < NB_LIGNE; j++){
+      if(){ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo  pas fin ici
+        return 1; // risque pour le roi
+      }
+    }
+  }
+  return 0; // pas de risque pour le roi
 }
