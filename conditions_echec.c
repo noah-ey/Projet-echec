09372eda7d534c1d37e8c24177c36d601d4e4587
Nicolas5u <nicolasdurand34080@gmail.com>
2025-01-15 11:40:20 +0100

    Update conditions_echec.c

--- conditions_echec.c
+++ conditions_echec.c
@@ -4,3 +4,5 @@ Fichier qui regroupe toutes les fonctions gérant les conditions d'échec
   fonction qui verif si le mouv que je viens de faire amene une condition d'echec avec une piece qui peut manger le roi maintenant que j'ai bougé la piece
   fonction qui bloque le mouvement d'une de mes pieces si elle amenerais un echec de mon roi
 */
+
+int fonction 
